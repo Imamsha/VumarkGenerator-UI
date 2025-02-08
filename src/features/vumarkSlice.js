168d09d23946c70ee97d3914bc7b40e6bdd8abd1
Imamsha <imamshas125@gmail.com>
2025-02-08 23:18:34 +0530

    url

--- src/features/vumarkSlice.js
+++ src/features/vumarkSlice.js
@@ -5,7 +5,7 @@ export const generateVuMark = createAsyncThunk(
   "vumark/generate",
   async (data) => {
     const response = await axios.post(
-      "https://13.57.107.17:8013/api/v1/vumark/generate", 
+      "http://13.57.107.17:8013/api/v1/vumark/generate", 
       data
     );
     return response.data;
